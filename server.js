3babc13769566cef1b74e4006c658d2a3d04f5b0
DKJazzu <dkjazzu@gmail.com>
2026-02-07 12:27:34 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.route('/').get((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Listening on port ${PORT}`);
 });
