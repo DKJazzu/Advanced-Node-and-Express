0315608aabf66d72ddc536596589b585cecada61
DKJazzu <dkjazzu@gmail.com>
2026-02-07 12:00:36 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const pug = require('pug');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'pug');
+app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.render(process.cwd() + 'views/pug/index.pug', {title: 'Hello', message: 'Please login'});
-  });
+app.route('/').get((req, res) => {
+  res.render('index', { title: 'Hello', message: 'Please log in' }); 
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
