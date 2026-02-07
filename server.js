fa26d8fa9c7e9dfb0cf2ae126ac3001dd0572318
DKJazzu <dkjazzu@gmail.com>
2026-02-07 10:25:24 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,22 +2,20 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
-require('pug');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('pug/index');
 });
-
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
