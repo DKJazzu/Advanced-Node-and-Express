06ccbcdedd6fba6effd76d0105299713c5483c28
DKJazzu <dkjazzu@gmail.com>
2026-02-07 10:47:02 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const pug = require('pug');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
 
 app.route('/').get((req, res) => {
   res.render('index');
